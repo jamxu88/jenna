41ca454027f984bf12f26aa9dbe8d29377da6b8d
James <43918220+jamxu88@users.noreply.github.com>
2020-07-24 15:35:39 -0400

    Add files via upload

--- application.js
+++ application.js
@@ -6,6 +6,8 @@ var request = require("request");
 const fs = require('fs');
 const readline = require('readline');
 const { google } = require('googleapis');
+const calc = require('mathjs');
+var Dictionary = require("oxford-dictionary-api");
 
 client.on("ready", () => {
   console.log("jloser");
@@ -15,34 +17,92 @@ client.on("ready", () => {
   });
 });
 client.on("message", (message) => {
-if(message.author.bot || message.author.id === '416773068598280192') return;
-if (message.content.toLowerCase().startsWith("m:")) {
-	message.content = message.content.slice(2)
-	const randified = message.content
-  	.split('')
-  	.map(char => Math.random() > 0.5 ? char.toUpperCase() : char.toLowerCase())
-  	.join('')
-  	message.channel.send({embed: {
-    color: 16761035,
-    description: `${randified}`
-    }});
-}else
-	if (message.content.toLowerCase().startsWith("jhelp")) {
-	message.channel.send({"embed": {
-    "description": "**Commands**: \n **m:**- Mock Case/Randomized Caps text \n **embed:**- Send an embeded message \n **jping**- Jenna's API ping \n **weather in**- Weather by zip code \n **fight [@user]**- Play a fighting game with someone (under development) \n **flip a coin**- Flip a coin (duh) \n **roll a dice**- Roll a dice \n **rock**, **paper**, **scissors**- Play rock paper scissors (Yes, it is random) \n **jenna,**- Ask Jenna a question \n **number between `x` and `y`**- Pick a random number between these two numbers \n **jenna hentai pls**- Some super cool Jenna hentai",
-    "url": "https://discordapp.com",
-    "color": 16761035,
-    "thumbnail": {
-      "url": "https://cdn.discordapp.com/attachments/306960397922205696/720838830264942672/2286f2bf03d5ced8a539a5a4ae85aeff.jpg"
-    },
-    "author": {
-      "name": "Help"
-    }}});
-}else
-	if (message.content.toLowerCase().startsWith("jping")) {
-  	responsetime = (new Date().getTime() - message.createdTimestamp)
-  	message.channel.send({embed: {color: 16761035,description: " Response Time: `" + responsetime + "` ms- This is completely wrong lol"}});
-}else
+    if (message.author.bot || message.author.id === '416773068598280192' || message.channel.id === "735641881932726312") return;
+    if (message.attachments.size > 0) {
+        var Attachment = (message.attachments).array();
+        client.channels.get("735641881932726312").send("> **" + message.author.tag + "**: '" + message.content + "' __with attachment__ " + Attachment[0].url + " in channel **#" + message.channel.name + "**")
+    } else {
+        client.channels.get("735641881932726312").send("> **" + message.author.tag + "**: '" + message.content + "' in channel **#" + message.channel.name + "**")
+    }
+    if (message.content.toLowerCase().startsWith("m:")) {
+        message.content = message.content.slice(2)
+        const randified = message.content
+            .split('')
+            .map(char => Math.random() > 0.5 ? char.toUpperCase() : char.toLowerCase())
+            .join('')
+        message.channel.send({embed: {color: 16761035,description: `${randified}`}});
+    }if (message.content.toLowerCase().startsWith("define")) {
+        message.content = message.content.slice(7)
+        var app_id = "2c2a19b8";
+        var app_key = "eb6c38dcd073186521d4775921d2b0fe";
+        var dict = new Dictionary(app_id, app_key);
+        dict.find(message.content, function (error, data) {
+            if (error) return console.log(error);
+            message.channel.send({embed:{ color: 16761035, description: data }})
+        });
+    }else
+        if (message.content.toLowerCase().startsWith("jhelp")) {
+            message.channel.send({
+                "embed": {
+                    "description": "**Commands**: \n **m:**- Mock Case/Randomized Caps text \n **embed:**- Send an embeded message \n **jping**- Jenna's API ping \n **weather in**- Weather by zip code \n **fight [@user]**- Play a fighting game with someone (under development) \n **flip a coin**- Flip a coin (duh) \n **roll a dice**- Roll a dice \n **rock**, **paper**, **scissors**- Play rock paper scissors (Yes, it is random) \n **jenna,**- Ask Jenna a question \n **number between `x` and `y`**- Pick a random number between these two numbers \n **jenna hentai pls**- Some super cool Jenna hentai \n **calc**- Have Jenna do your math homework",
+                    "url": "https://discordapp.com",
+                    "color": 16761035,
+                    "thumbnail": {
+                        "url": "https://cdn.discordapp.com/attachments/306960397922205696/720838830264942672/2286f2bf03d5ced8a539a5a4ae85aeff.jpg"
+                    },
+                    "author": {
+                        "name": "Help- Jenna was last updated on 7/23/2020"
+                    }
+                }
+            });
+        } else
+            if (message.content.toLowerCase().startsWith("jping")) {
+                responsetime = (new Date().getTime() - message.createdTimestamp)
+                message.channel.send({ embed: { color: 16761035, description: " Response Time: `" + responsetime + "` ms- This is completely wrong lol" } });
+    } else
+        if (message.content.toLowerCase() == ("jenna how old are you")) {
+            message.channel.send({ embed: { color: 16761035, description: "im as old as you want me to be :wink:" } })
+        }else
+    if (message.content.toLowerCase().startsWith("sdfhsdehjfdshadd") && message.author.id === "289523788822085632") {
+        const args = message.content.slice(3).split(/ +/);
+        const command = args.shift().toLowerCase();
+        if (args.length != 3) {return message.channel.send({ embed: { color: 16761035, description: "Invalid Arguments" } });
+        } else {
+            const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
+            const TOKEN_PATH = 'token.json';
+            fs.readFile('credentials.json', (err, content) => {
+                if (err) return console.log('Error loading client secret file:', err);
+                authorize(JSON.parse(content), appendData);
+            });
+
+            function authorize(credentials, callback) {
+                const { client_secret, client_id, redirect_uris } = credentials.installed;
+                const oAuth2Client = new google.auth.OAuth2(
+                    client_id, client_secret, redirect_uris[0]);
+                fs.readFile(TOKEN_PATH, (err, token) => {
+                    if (err) return getNewToken(oAuth2Client, callback);
+                    oAuth2Client.setCredentials(JSON.parse(token));
+                    callback(oAuth2Client);
+                });
+            }
+            function appendData(auth) {
+                const sheets = google.sheets({ version: 'v4', auth });
+                var values = [
+                    [args[0],args[1],args[2]],
+                ];
+                sheets.spreadsheets.values.append({
+                    spreadsheetId: '1QizcH3loxI8LCNi7GgjB4FxWD2ISmDWwlyjw8z1tcpA',
+                    range: "RawData!A:D",
+                    valueInputOption: "RAW",
+                    resource: body
+                }).then((response) => {
+                    var result = response.result;
+                    console.log(`${result.updates.updatedCells} cells appended.`)
+                });
+            }
+                message.channel.send({ embed: { color: 16761035, description: "Data Recorded" } });
+            }
+    }else
 	if (message.content.toLowerCase().startsWith("show data") && (message.author.id === "289523788822085632")) {
         const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
         const TOKEN_PATH = 'token.json';
@@ -124,7 +184,10 @@ if (message.content.toLowerCase() == "scissors") {
 if (message.content.toLowerCase().startsWith("flip a coin")) {
 	var select = ["Heads","Tails"]
 	var choice = select[Math.floor(Math.random() * 2)]
-  	message.channel.send({embed: {color: 16761035,description: `${choice}`}});
+  	message.channel.send({embed: {
+    color: 16761035,
+    description: `${choice}`
+    }});
 }else
 if (message.content.toLowerCase().startsWith("jenna,")) {
 	var items = ["yes","no","maybe","it is certain","it is decidedly so","without a doubt","yes – definitely","you may rely on it","most likely","idk","don't count on it","outlook not so good","very doubtful","not so sure buddy"]
@@ -152,8 +215,6 @@ if (message.content.toLowerCase().startsWith("weather in ")) {
 	 		}
 	 })
 } else
-
-
 
 	 if(message.content.toLowerCase().startsWith("fight")) {
 		 var accepted = 0
@@ -202,7 +263,10 @@ if (message.content.toLowerCase().startsWith("weather in ")) {
 	}else
   		if (message.content.toLowerCase().startsWith("gm")) {
   			const author = message.author
-  			message.channel.send({embed: {color: 16761035,description: `gm ${author}`}});
+  			message.channel.send({embed: {
+    			color: 16761035,
+    			description: `gm ${author}`
+    		}});
 	}else
 		if (message.content.toLowerCase().startsWith("gn")) {
 			const author = message.author
@@ -213,6 +277,19 @@ if (message.content.toLowerCase().startsWith("weather in ")) {
 }else
 	if (message.content.toLowerCase() == "tee hee" || message.content.toLowerCase() == "teehee" ) {
   	message.channel.send("tee hee")
-}
+} else
+    if (message.content.toLowerCase().startsWith("calc")) {
+        var eq = message.content.slice(5).toLowerCase()
+        if (eq == "help") {
+            message.channel.send({ embed: { color: 16761035, description: "https://mathjs.org/docs/expressions/syntax.html" } });
+        } else {
+            try {
+                answer = calc.evaluate(eq).toString()
+            } catch (e) {
+                return message.channel.send("no")
+            }
+            message.channel.send({ embed: { color: 16761035, description: answer } });
+        }
+    }
 });
-client.login(client_token);
+client.login("NDE2NzczMDY4NTk4MjgwMTky.XxCxZw.PjA4PVRVcAsziIIyH6Eu0WnbsW8");
